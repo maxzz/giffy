39e600c2de8062bf07eb37ff10fb8db77b57ad63
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-17 17:52:13 -0700

    WIP: Tech stack update. 7. background fill

--- src/hooked-serach.tsx
+++ src/hooked-serach.tsx
@@ -33,34 +33,35 @@ export default function HookedSearch() {
     const [results, loading] = useGiphy(query);
 
     return (
-        <React.Fragment>
-            <div className="h-16 px-4 flex items-center justify-end text-gray-100 bg-gradient-to-r from-gray-400 to-gray-800 uppercase">
-                <h1 className="text-2xl">Giffy Search</h1>
-            </div>
-            <div className="container max-w-md mx-auto bg-gray-600">
-                <form
-                    className="mt-4 flex flex-col"
-                    onSubmit={e => {
-                        e.preventDefault();
-                        setQuery(search);
-                    }}
-                >
-                    <input className="px-4 py-2 border rounded-sm text-gray-800"
-                        value={search} onChange={e => setSearch(e.target.value)} placeholder="What GIF to search for" />
-                    <button className="self-center mt-4 px-4 py-1 border rounded-md text-gray-800">Search</button>
-                </form>
+        <div className="bg-gray-600">
+            <div className="h-screen">
 
-                <div className="mt-4">
-                    {loading
-                        ? 'Loading'
-                        : <div className="grid" style={{gridTemplateColumns: 'repeat(4, minmax(100px, 1fr))'}}>
-                                {results.map(item => {
-                                    return <video autoPlay loop key={item} src={item} />;
-                                })}
-                        </div>
-                    }
+                <div className="h-16 px-4 flex items-center justify-end text-gray-100 bg-gradient-to-r from-gray-400 to-gray-800 shadow-md">
+                    <h1 className="text-2xl uppercase">Giffy Search</h1>
+                </div>
+
+                <div className="container max-w-md mx-auto">
+                    <form
+                        className="mt-4 flex flex-col"
+                        onSubmit={e => { e.preventDefault(); setQuery(search); }}
+                    >
+                        <input className="px-4 py-2 border rounded-sm text-gray-800"
+                            value={search} onChange={e => setSearch(e.target.value)} placeholder="What GIF to search for" />
+                        <button className="self-center mt-4 px-4 py-1 border rounded-md text-gray-300">Search</button>
+                    </form>
+
+                    <div className="mt-4">
+                        {loading
+                            ? 'Loading'
+                            : <div className="grid" style={{gridTemplateColumns: 'repeat(4, minmax(100px, 1fr))'}}>
+                                    {results.map(item => {
+                                        return <video autoPlay loop key={item} src={item} />;
+                                    })}
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
     );
 }
