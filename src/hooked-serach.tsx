342fa36d124a1a13be838e18b4d29d5261954746
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-18 03:17:27 -0700

    WIP: scroll area. 3. done. now on grid

--- src/hooked-serach.tsx
+++ src/hooked-serach.tsx
@@ -35,15 +35,16 @@ export default function HookedSearch() {
     const [results, loading] = useGiphy(query);
 
     return (
-        <div className="">
-
-            <div className="h-16 px-4 flex items-center justify-end text-gray-100 bg-gradient-to-r from-gray-400 to-gray-800 shadow-md">
+        <div className="flex flex-col h-screen">
+            {/* Header */}
+            <div className="h-16 px-4 flex-none flex items-center justify-end text-gray-100 bg-gradient-to-r from-gray-400 to-gray-800 shadow-md">
                 <h1 className="text-2xl uppercase">Giffy Search</h1>
             </div>
 
-            <div className="container max-w-md mx-auto flex flex-col">
+            <div className="container overflow-hidden flex-auto max-w-md mx-auto flex flex-col">
+                {/* Search form */}
                 <form
-                    className="mt-4 flex flex-col"
+                    className="mt-4 flex-none flex flex-col"
                     onSubmit={e => { e.preventDefault(); setQuery(search); }}
                 >
                     <input className="px-4 py-2 border rounded-sm text-gray-800"
@@ -58,10 +59,11 @@ export default function HookedSearch() {
                     >Search</button>
                 </form>
 
-                <div className="mt-4 max-h-96 overflow-y-auto smallscroll">
+                {/* Search results */}
+                <div className="mt-4 flex-auto overflow-y-auto smallscroll">
                     {loading
                         ? 'Loading'
-                        : <div className="overflow-y-auto" style={{ columnCount: 4, columnGap: '.5rem' }}>
+                        : <div className="" style={{ columnCount: 4, columnGap: '.5rem' }}>
                             {results.map(item => {
                                 return <video className="mb-2" autoPlay loop key={item} src={item} />;
                             })}
@@ -69,21 +71,23 @@ export default function HookedSearch() {
                     }
                 </div>
 
-                <div className="fixed flex right-2 bottom-3">
-                    <a
-                        className="p-1 pt-2 pb-0 border rounded-full text-gray-500 border-gray-500"
-                        href="https://github.com/maxzz/giffy" target="_blank"
-                        title="Open GitHub source code"
-                    >
-                        <div className="w-5 h-5">
-                            <svg className="stroke-1" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
-                                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                            </svg>
-                        </div>
-                    </a>
-                </div>
-
             </div>
+
+            {/* GitHUb */}
+            <div className="fixed flex-none flex right-2 bottom-3">
+                <a
+                    className="p-1 pt-2 pb-0 border rounded-full text-gray-500 border-gray-500"
+                    href="https://github.com/maxzz/giffy" target="_blank"
+                    title="Open source code on GitHub"
+                >
+                    <div className="w-5 h-5">
+                        <svg className="stroke-1" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                        </svg>
+                    </div>
+                </a>
+            </div>
+
         </div>
     );
 }
